0af3285776cc81fb9224eddb145e8d2890a5e62e
sdcb <sdflysha@qq.com>
2023-11-17 19:14:45 +0800

    add nlgl

--- ffmpeg-wjz-sorry-generator/Controllers/SorryController.cs
+++ ffmpeg-wjz-sorry-generator/Controllers/SorryController.cs
@@ -31,8 +31,9 @@ namespace ffmpeg_wjz_sorry_generator.Controllers
             FFmpegLogger.LogWriter = (level, msg) => _logger.LogInformation(msg);
             Mp4SourceDef def = type switch
             {
-                "wjz" => Mp4SourceDef.WangJingZe, 
-                "sorry" => Mp4SourceDef.Sorry, 
+                "wjz" => Mp4SourceDef.WangJingZe,
+                "sorry" => Mp4SourceDef.Sorry,
+                "nlgl" => Mp4SourceDef.Nlgl,
                 _ => throw new NotImplementedException($"Not supported type: {type}"),
             };
             Mp4Source mp4Source = def.CreateLines(subtitle.Split("|"));
@@ -220,6 +221,31 @@ namespace ffmpeg_wjz_sorry_generator.Controllers
             new SubtitleDef(13.81, 16.31, "Sorry"),
             new SubtitleDef(18.06, 19.56, "有钱真的了不起"),
             new SubtitleDef(19.60, 21.60, "不过我想你不会明白这种感觉"),
+        });
+
+        public static readonly Mp4SourceDef Nlgl = new Mp4SourceDef("https://io.starworks.cc:88/cv-public/2023/nlgl-mini.mp4", new[]
+        {
+            new SubtitleDef(0.333, 1.399, "花西子越来越贵了"),
+            new SubtitleDef(1.400, 2.333, "哪里贵了"),
+            new SubtitleDef(2.766, 4.733, "这么多年都是这个价格好吧"),
+            new SubtitleDef(4.733, 6.166, "不要睁着眼睛乱说"),
+            new SubtitleDef(6.166, 8.966, "国货品牌很难的哦"),
+            new SubtitleDef(8.966, 10.199, "而且花西子真的不是那种"),
+            new SubtitleDef(10.200, 11.700, "随便买原料就做的品牌"),
+            new SubtitleDef(11.700, 13.966, "哎我跟花西子跟了多少年"),
+            new SubtitleDef(13.966, 15.899, "他怎么起来的我是最知道的一个人"),
+            new SubtitleDef(15.900, 16.700, "是啊"),
+            new SubtitleDef(17.100, 18.966, "他们就差点把他们家掏给我了"),
+            new SubtitleDef(19.100, 19.900, "哈哈好不好"),
+            new SubtitleDef(19.900, 20.600, "真的乱说"),
+            new SubtitleDef(20.600, 22.266, "这么多年都是79块钱"),
+            new SubtitleDef(22.266, 23.333, "哪里贵了"),
+            new SubtitleDef(23.366, 24.899, "有的时候找找自己原因好吧"),
+            new SubtitleDef(24.900, 25.333, "这么多年了"),
+            new SubtitleDef(25.333, 25.999, "工资涨没涨"),
+            new SubtitleDef(26.000, 28.200, "有没有认真工作好不好"),
+            new SubtitleDef(28.800, 29.966, "这么多年都是这个价格"),
+            new SubtitleDef(29.966, 30.899, "我真的快疯掉了"),
         });
     }
 
